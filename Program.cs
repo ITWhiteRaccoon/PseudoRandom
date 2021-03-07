b7ed6bba95732cc2fab952337d5141c946d62b3d
Eduardo C. Andrade <eduardo.andrade@pucrs.br>
2021-03-06 21:14:17 -0300

    Added Middle Square exercise

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace PseudoRandom
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ExMiddleSquare(5437, 9);
+        }
+
+        static void ExMiddleSquare(int seed, int iter)
+        {
+            for (int i = 0; i <= iter - 1; i++)
+            {
+                string strResult = ((int) Math.Pow(seed, 2)).ToString("D8");
+                Console.WriteLine(strResult);
+                seed = Convert.ToInt32(strResult.Substring(2, 4));
+            }
         }
     }
 }
